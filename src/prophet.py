f6890dff3db1d11d19f913105503a6216549fc6e
Yuya Jeremy Ong <yuyajeremyong@gmail.com>
2018-11-28 02:06:16 -0500

    (add) Append ARIMA models

--- src/prophet.py
+++ src/prophet.py
@@ -5,36 +5,58 @@ Author: Yuya Ong
 from __future__ import print_function
 import os
 import sys
+import numpy as np
 import pandas as pd
 from fbprophet import Prophet
 import matplotlib.pyplot as plt
+
+def mape(y_true, y_pred):
+    return np.mean(np.abs((y_true - y_pred) / y_true)) * 100
 
 # Load Dataset
 birth_rate = pd.read_csv('../data/raw/birth_rate.csv', index_col=1)
 birth_rate = birth_rate.drop(['Country Name', 'Indicator Name', 'Indicator Code', '2017', 'Unnamed: 62'], axis=1)
 birth_rate = birth_rate.dropna().T
 
+# MAPE Output
+err = open('../log/ARIMA_MAPE.csv', 'w')
+
 # Setup Data Structure
-data = birth_rate['RUS'].to_frame()
-data = data.rename(columns={'RUS':'y'})
-data['ds'] = pd.Series(map(lambda x: str(x) + '-01-01', list(data.T)), index=data.index)
+for country in list(birth_rate):
+    data = birth_rate[country].to_frame()
+    data = data.rename(columns={country:'y'})
+    data['ds'] = pd.Series(map(lambda x: str(x) + '-01-01', list(data.T)), index=data.index)
 
-# Swap Column Order
-cols = data.columns.tolist()
-cols = cols[-1:] + cols[:-1]
-data = data[cols]
+    # Swap Column Order
+    cols = data.columns.tolist()
+    cols = cols[-1:] + cols[:-1]
+    data = data[cols]
 
-print(data.head())
+    # Train Model
+    m = Prophet(yearly_seasonality=False, daily_seasonality=False, weekly_seasonality=False)
+    m.fit(data[:-20])
 
-# Train Model
-m = Prophet(yearly_seasonality=False, daily_seasonality=False, weekly_seasonality=False)
-m.fit(data)
+    # Generate Prediction
+    future = m.make_future_dataframe(periods=20, freq='Y')
+    forecast = m.predict(future)
 
-future = m.make_future_dataframe(periods=25, freq='Y')
-print(future.tail())
+    # Compute Error
+    error = mape(data['y'].values, forecast['trend'].values)
+    err.write(country+','+str(error)+'\n')
 
-forecast = m.predict(future)
-print(forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail())
+    # Generate Plots
+    print(forecast.head())
+    out = np.array([forecast['trend'].tolist(), data['y'].tolist()])
+    plt.plot(out.T)
+    plt.xticks(plt.xticks()[0], list(range(1950, 2016, 10)))
 
-fig1 = m.plot(forecast)
-plt.show(fig1)
+    plt.suptitle('Birth Rate Projection ['+country+'] (ARIMA)')
+    plt.title('MAPE: ' + str("{0:.2f}".format(error)) + '%')
+    plt.legend(['Predicted', 'Actual'])
+    plt.xlabel('Year')
+    plt.ylabel('Birth Rate (Per Woman)')
+
+    plt.savefig('../plot/ARIMA/'+country+'.png')
+    plt.clf()
+
+err.close()
